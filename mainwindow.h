2796e21ed2544133b2103e27dfa3be96bd1ce205
LiuShenLan <liushenlan1997@gmail.com>
2022-01-09 12:00:06 +0800

    camera load bebug

--- mainwindow.h
+++ mainwindow.h
@@ -34,7 +34,7 @@
 #define CAM_LOAD_DRONE_CAMERA	2
 #define CAM_LOAD_PC_VIDEO		"../../dataset/dronet test video/all copy.mp4"
 // 摄像头读取选择
-#define CAM_LOAD	CAM_LOAD_PC_CAMERA
+#define CAM_LOAD	CAM_LOAD_DRONE_CAMERA
 // 摄像头显示选择
 #define CAM_SHOW_DETECT_CAMERA_FLAG	true
 #define CAM_SHOW_DETECT_CAMERA		"../../dataset/hostData/detece result/show_realTimeImg.jpg"
